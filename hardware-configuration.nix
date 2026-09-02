3b40498d6c2813e3b17a1699c21c95dc34d407a9
plabklapp <alebetco@gmail.com>
2026-09-02 16:19:02 -0500

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,25 +14,18 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3d51420f-b1d2-43de-b6b1-b974c8f96acb";
-      fsType = "btrfs";
-      options = [ "subvol=@" "compress=zstd" ];
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/3d51420f-b1d2-43de-b6b1-b974c8f96acb";
-      fsType = "btrfs";
-      options = [ "subvol=@home" "compress=zstd" ];
+    { device = "/dev/disk/by-uuid/8a2e940f-3fa8-4818-b0ba-026147daac90";
+      fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2F5C-AF19";
+    { device = "/dev/disk/by-uuid/DA7E-D304";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/4c7129a8-6943-41f4-8df4-240112281751"; }
+    [ { device = "/dev/disk/by-uuid/78d77bd0-03bc-4014-8cc0-278943103769"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
